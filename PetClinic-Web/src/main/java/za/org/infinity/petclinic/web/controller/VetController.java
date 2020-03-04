83cf3dba20b3ab967543121615e66ae9f51d1f29
Piet <pietvdw67@gmail.com>
2020-03-04 18:41:22 +0200

    Added vet list and updated controller. Closes #16

--- PetClinic-Web/src/main/java/za/org/infinity/petclinic/web/controller/VetController.java
+++ PetClinic-Web/src/main/java/za/org/infinity/petclinic/web/controller/VetController.java
@@ -1,13 +1,24 @@
 package za.org.infinity.petclinic.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import za.org.infinity.petclinic.data.service.VetService;
 
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+	
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets",vetService.findAll());
 		
 		return "vets/index";
 	}
