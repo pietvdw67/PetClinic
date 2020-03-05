945cf576e07be3e59b20469ae3553abb426ee432
Piet <pietvdw67@gmail.com>
2020-03-05 18:40:45 +0200

    Added speciality pojo and added speciality to vet. Closes #28

---
+++ PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/model/Speciality.java
@@ -0,0 +1,17 @@
+package za.org.infinity.petclinic.data.model;
+
+public class Speciality extends BaseEntity {
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
