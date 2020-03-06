c9a923a16f9b154618d58c74fe6987b4910773d0
Piet <pietvdw67@gmail.com>
2020-03-06 16:39:38 +0200

    Created speciality map service and implementation. Closes #32

---
+++ PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/service/SpecialityService.java
@@ -0,0 +1,7 @@
+package za.org.infinity.petclinic.data.service;
+
+import za.org.infinity.petclinic.data.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+
+}
