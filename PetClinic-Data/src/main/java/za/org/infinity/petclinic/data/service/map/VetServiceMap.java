3b6c1cc8daf34d664e09b14c59e07899b60578c0
Piet <pietvdw67@gmail.com>
2020-03-06 17:04:11 +0200

    Added save to vetservicemap. Part of #35

--- PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/service/map/VetServiceMap.java
+++ PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/service/map/VetServiceMap.java
@@ -4,12 +4,20 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import za.org.infinity.petclinic.data.model.Speciality;
 import za.org.infinity.petclinic.data.model.Vet;
+import za.org.infinity.petclinic.data.service.SpecialityService;
 import za.org.infinity.petclinic.data.service.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
 
+	private final SpecialityService specialityService;
+	
+	public VetServiceMap(SpecialityService specialityService) {
+		this.specialityService = specialityService;
+	}
+	
 	@Override
 	public Vet findById(Long id) {
 		return super.findById(id);
@@ -22,6 +30,16 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
 	@Override
 	public Vet save(Vet object) {
+		
+		if (object.getSpecialities().size() > 0) {
+			object.getSpecialities().forEach(speciality -> {
+				if (speciality.getId() == null) {
+					Speciality savedSpeciality = specialityService.save(speciality);
+					speciality.setId(savedSpeciality.getId());
+				}
+			});
+		}
+		
 		return super.save(object);
 	}
 
