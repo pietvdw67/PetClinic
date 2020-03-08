eb07e9f7f90ee69437d67987d29dfd0b83a898ec
Piet <pietvdw67@gmail.com>
2020-03-08 08:43:32 +0200

    Implemented base entity. closes #36

--- PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/model/BaseEntity.java
+++ PetClinic-Data/src/main/java/za/org/infinity/petclinic/data/model/BaseEntity.java
@@ -2,8 +2,16 @@ package za.org.infinity.petclinic.data.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 
 	public Long getId() {
